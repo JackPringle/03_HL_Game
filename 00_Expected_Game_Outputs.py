de7b1027d37cc8b6a9cb059def75a8629c7630c5
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-04-05 12:06:59 +1200

    From video 28 (what finished game should output)

---
+++ 00_Expected_Game_Outputs.py
@@ -0,0 +1,57 @@
+# What playing the game should look like when running the final code
+
+# ------------------------------------------------------------------
+
+# **** Welcome to the Higher Lower Game ****
+
+# For each game you will be asked to...
+# - Enter a 'low' and 'high' number. The computer will randomly generate a 'secret'
+# number between your two chosen numbers. It will us these numbers for all
+# the rounds in a given game.
+# - The computer will calculate how many guesses you are allowed
+# - enter the number of rounds you want to play
+# - guess the secret number
+
+# Good Luck!
+
+# Please press <enter> to begin...
+
+# Low Number: xlii
+# Please enter an integer
+# Low Number: 1
+# High Number: xlii
+# Please enter an integer that is more than or equal to 2
+#  High Number: 1
+# Please enter an integer that is more than or equal to 2
+# High Number: 2
+# Round: 1
+
+# ###################################################
+# Round 1 of 1 - you have 2 guesses left...
+# ###################################################
+
+# Guess a number between 1 and 2: 1
+
+# *******************************************
+# Amazing! You got it in one guess
+# *******************************************
+
+# ------------------------------------
+# Won: 1    |     Lost: 0
+
+# Please press enter to see your game summary / statistics
+
+# **** Game Scores ****
+# Round 1: 1(won)
+
+# **** Summary Statistics ****
+# Best: 1
+# Worse: 1
+# Average: 1.00
+
+# Press <enter> to play again or any key quit:
+
+# THEN LOOP FROM ENTER LOW NUMBER:
+
+
+
