7dc4f2ca165d5fb2972fcab35f21c7061767bb5b
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-05-19 10:02:24 +1200

    fixed and works

--- 05_HL_no_duplicates.py
+++ 05_HL_no_duplicates.py
@@ -1,11 +1,46 @@
-# HL component 5 - no duplicates
-
-# To Do
-# set up empty list called already_guessed
-# when user guesses, add guess to list
-# for each guess, check that number is not in already_guessed
-
 # HL component 5 - Prevents duplicate guesses
+
+# Functions...
+
+# Integer checker
+def int_check(question, low=None, high=None, exit_code=None):
+    if low is None and high is None:
+        error = "Please enter an integer"
+        situation = "any integer"
+
+    elif low is not None and high is not None:
+        error = f"Please enter an integer between {low} and {high}"
+        situation = "both"
+    else:
+        error = f"Please enter an integer more than {low}"
+        situation = "low only"
+
+    while True:
+        response = input(question).lower()
+        if response == exit_code:
+            return response
+
+        try:
+            response = int(response)
+
+            # check that integer is valid (ie: not too low / too high etc...)
+            if situation == "any integer":
+                return response
+
+            elif situation == "both":
+                if low <= response <= high:
+                    return response
+
+            elif situation == "low only":
+                if response >= low:
+                    return response
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
 
 SECRET = 7
 GUESSES_ALLOWED = 5
@@ -18,7 +53,8 @@ guess = ""
 
 while guess != SECRET and guesses_left >= 1:
 
-    guess = int(input("Guess: "))  # replace this with function
+    guess = int_check("Guess: ")
+    print()
 
     # checks that guess is not duplicate
     if guess in already_guessed:
@@ -26,25 +62,37 @@ while guess != SECRET and guesses_left >= 1:
         print(f"You *still* have {guesses_left} guesses left")
         continue
 
-        guesses_left -= 1
-        already_guessed.append(guess)
+    guesses_left -= 1
+    already_guessed.append(guess)
 
-        if guesses_left >= 1:
+    if guesses_left >= 1:
 
-            if guess < SECRET:
-                print(f"Too low, try a higher number. Guesses left: {guesses_left}")
+        if guess < SECRET:
+            print(f"TOO LOW, try a higher number.")
+            print()
+            print(f"Guesses left: {guesses_left}")
+            print()
 
-            elif guess > SECRET:
-                print(f"Too high, try a lower number. Guesses left: {guesses_left}")
+        elif guess > SECRET:
+            print(f"TOO HIGH, try a lower number.")
+            print()
+            print(f"Guesses left: {guesses_left}")
+            print()
 
-        else:
-            if guess < SECRET:
-                print("Too Low!")
-            elif guess > SECRET:
-                print("Too High!")
+    else:
+        if guess < SECRET:
+            print("Too Low!")
+        elif guess > SECRET:
+            print("Too High!")
 
-    if guess == SECRET:
-        if guesses_left == GUESSES_ALLOWED:
-            print("Amazing! You got it! ")
-        else:
-            print("Well done, you got it!")
+        if guess == SECRET:
+            if guesses_left == GUESSES_ALLOWED:
+                print("Amazing! You got it! ")
+            else:
+                print("Well done, you got it!")
+
+if guesses_left == 0:
+    print()
+    print("----------------------------")
+    print("NO MORE GUESSES!")
+    print("----------------------------")
