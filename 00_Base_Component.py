ae98f287c3c97af29a48369dce772ab3616e9d2d
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-05-19 10:01:11 +1200

    Added 05 guessing

--- 00_Base_Component.py
+++ 00_Base_Component.py
@@ -1,13 +1,15 @@
-# Functions
+# Functions...
 
 # Checks an integer is valid (more than low, less than high)
 def int_check(question, low=None, high=None, exit_code=None):
     if low is None and high is None:
         error = "Please enter an integer"
         situation = "any integer"
-    if low is not None and high is not None:
+
+    elif low is not None and high is not None:
         error = f"Please enter an integer between {low} and {high}"
         situation = "both"
+
     else:
         error = f"Please enter an integer more than {low}"
         situation = "low only"
@@ -38,23 +40,23 @@ def int_check(question, low=None, high=None, exit_code=None):
             print(error)
 
 
-# Main Routine
+# Main Routine...
 
 rounds_played = 0
 rounds_won = 0
 
 low_number = 1
-high_number = 10
+high_number = 100
 
 mode = "regular"
 
-rounds = int_check("How many rounds", 1, exit_code="")
+rounds = int_check("How many rounds?: ", 1, exit_code="")
 
 if rounds == "":
     mode = "infinite"
     rounds = 5
 
-# Rounds loop
+# Rounds loop...
 end_game = "no"
 while end_game == "no":
 
@@ -68,25 +70,59 @@ while end_game == "no":
 
     rounds_played += 1
 
-    # Start Round!!
-    while True:
+    SECRET = 7
+    GUESSES_ALLOWED = 5
+    guesses_left = GUESSES_ALLOWED
+    already_guessed = []
 
-        secret = 7
+    # Start  guessing!
 
-        guess = int_check("Guess (or 'xxx' to exit): ", low_number, high_number, "xxx")
-        print("you guessed", guess)
+    guess = ""
 
-        if guess == "xxx":
-            end_game = "yes"
-            break
+    while guess != SECRET and guesses_left >= 1:
 
-            # compare guess to secret number
-        print("Pretend we've compared")
+        guess = int_check("Guess: ")
+        print()
 
-        if guess == secret:
-            rounds_won += 1
-            break
+        # checks that guess is not duplicate
+        if guess in already_guessed:
+            print("You already guessed that number! Please try again.")
+            print(f"You *still* have {guesses_left} guesses left")
+            continue
 
-    # check if we are out of rounds
-    if rounds_played >= rounds:
+        guesses_left -= 1
+        already_guessed.append(guess)
+
+        if guesses_left >= 1:
+
+            if guess < SECRET:
+                print(f"TOO LOW, try a higher number.")
+                print()
+                print(f"Guesses left: {guesses_left}")
+                print()
+
+            elif guess > SECRET:
+                print(f"TOO HIGH, try a lower number.")
+                print()
+                print(f"Guesses left: {guesses_left}")
+                print()
+
+        else:
+            if guess < SECRET:
+                print("Too Low!")
+            elif guess > SECRET:
+                print("Too High!")
+
+            if guess == SECRET:
+                if guesses_left == GUESSES_ALLOWED:
+                    print("Amazing! You got it! ")
+                else:
+                    print("Well done, you got it!")
+
+    if guesses_left == 0:
+        print()
+        print("----------------------------")
+        print("NO MORE GUESSES!")
+        print("----------------------------")
         break
+
